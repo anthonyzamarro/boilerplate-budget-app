4f3c882b05bc50608592c7ee3612028262083358
Anthony <anthony.zamarro@gmail.com>
2021-03-02 22:14:02 -0500

    fix and complete formatted Category class output. Begin work on create_spend_chart function

--- budget.py
+++ budget.py
@@ -37,20 +37,52 @@ class Category:
 		stars_around_name =  num_stars / 2 * '*'
 		return stars_around_name + self.category + stars_around_name
 	
+	def format_total(self, ):
+		return 'Total: ' + str(self.get_balance())
+		
+	
 	def format_description(self, ):
 		descriptions = []
 		for desc in self.ledger:
-			desc_spaces_len = 23 - len(desc['description'])	
-			descriptions.append(desc['description'] + desc_spaces_len * ' ' + str(desc['amount']) + '.00\n')
+			# description
+			desc_len = len(desc['description'])
+			if desc_len >= 23:
+				desc['description'] = desc['description'][:23]	
+			# amount
+			amount = desc['amount']
+			if isinstance(amount, float) == False:
+				amount = str(desc['amount']) + '.00'
+			amount = str(amount) 
+			amount_len = len(amount)
+			# spaces
+			total_spaces_len = amount_len + len(desc['description'])
+			spaces = 1
+			if total_spaces_len < 30:
+				spaces = 30 - total_spaces_len
+			descriptions.append(desc['description'] + spaces * ' ' + amount)
 		return descriptions
 
 	def __repr__(self):
 		category_name = self.format_category_name()
 		description = self.format_description()
-		return category_name +  '\n' + ''.join(description)
-#def create_spend_chart(categories):
+		total = self.format_total()
+		return category_name +  '\n' + '\n'.join(description) + '\n' + total
 
-
+def create_spend_chart(categories):
+	withdraws = []
+	cat_names = []
+	for category in categories:
+		split = str(category).split()
+		for s in split:
+			if '*' in s:
+				cat_names.append(s)
+			try:
+				if float(s) < 0:
+					withdraws.append(float(s))
+			except ValueError:
+				pass
+	print withdraws
+	print cat_names
 if __name__ == '__main__':
 	food = Category('Food')
 	clothing = Category('Clothing')
@@ -59,7 +91,9 @@ if __name__ == '__main__':
 	clothing.withdraw(200, 'shoes')
 
 	food.deposit(1000, 'lots of apples')
-	food.withdraw(100, 'bought apples')
-	food.transfer(200, clothing)
-	print repr(food)
-	print repr(clothing)
+	food.withdraw(10.15, 'bought apples')
+	food.withdraw(15.89, 'restaurant and more apples')
+	food.transfer(50, clothing)
+	#print repr(food)
+	#print repr(clothing)
+	create_spend_chart([food, clothing])
