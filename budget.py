206f7874280f704f6a278b65628087c12994ce6e
Anthony <anthony.zamarro@gmail.com>
2021-03-08 21:09:23 -0500

    finish spending chart

--- budget.py
+++ budget.py
@@ -40,7 +40,6 @@ class Category:
 	def format_total(self, ):
 		return 'Total: ' + str(self.get_balance())
 		
-	
 	def format_description(self, ):
 		descriptions = []
 		for desc in self.ledger:
@@ -78,51 +77,55 @@ def create_spend_chart(categories):
 
 	# group category name and withdraws
 	cats = map(get_cats, categories)
+	result = 'Percentage spent by category\n'
 
-	# add up all spendings and divide each category spending by total spendings
+	# total = sum(x.spent for x in categories)
 	total_spending = []
 	for s in cats:
 		total_spending.append(s[1])
 
-	# get percentage list -- (category, total money spent for this category)
+	# percentages = [(x.spent/total)//0.01 for x in categories]
+	# get percentage list
 	def get_percent(p):
 		percent = int(round(p[1] / sum(total_spending) * 100)) / 10
-		return p[0], percent
+		return percent * 10
+		# return range(percent - 1, -1,-1)
 
-	# (catgory name, percentage spent of total)
-	cat_percent = map(get_percent, cats)
+	# returns (catgory name, percentage spent of total)
+	percentages = map(get_percent, cats)
 
-	def format_y_axis(num):
-		return str(num) + '|\n'
+	for x in range(100, -10, -10):
+	    result = result + str(x).rjust(3, " ") + '|'
+	    for y in percentages:
+	        if y >= x:
+	            result = result + ' o '
+	        else:
+	            result = result + '   '
+	    result = result + ' \n'
+	result = result + '    ' + '-'*len(percentages)*3 + '-\n'
+	maxLength = max(len(x.category) for x in categories)
+	for x in range(maxLength):
+	    result = result + '    '
+	    for y in categories:
+	        if x < len(y.category):
+	            result = result + ' ' + y.category[x] + ' '
+	        else:
+	            result = result + '   '
+	    result = result + ' \n'
+	return result.rstrip() +'  '
 
-	y_axis_nums = map(format_y_axis, range(100, -10, -10))
 
-	def format_vertical_name_percent(n):
-		o_list = n[1] * 'o \n'
-		c_list = ''
-		for l in n[0]:
-			c_list += l + '\n'
-		return o_list + '-\n'+ c_list
-	vertical_cat = map(format_vertical_name_percent, cat_percent)
+# if __name__ == '__main__':
+# 	food = Category('Food')
+# 	clothing = Category('Clothing')
 
-	for x, i in enumerate(y_axis_nums):
-		try:
-			print i, vertical_cat[x]
-		except IndexError:
-			pass
+# 	clothing.deposit(1000, 'pants')
+# 	clothing.withdraw(200, 'shoes')
 
-
-if __name__ == '__main__':
-	food = Category('Food')
-	clothing = Category('Clothing')
-
-	clothing.deposit(1000, 'pants')
-	clothing.withdraw(200, 'shoes')
-
-	food.deposit(1000, 'lots of apples')
-	food.withdraw(10.15, 'bought apples')
-	food.withdraw(15.89, 'restaurant and more apples')
-	food.transfer(50, clothing)
-	#print repr(food)
-	#print repr(clothing)
-	create_spend_chart([food, clothing])
+# 	food.deposit(1000, 'lots of apples')
+# 	food.withdraw(10.15, 'bought apples')
+# 	food.withdraw(15.89, 'restaurant and more apples')
+# 	food.transfer(50, clothing)
+# 	#print repr(food)
+# 	#print repr(clothing)
+# 	create_spend_chart([food, clothing])
